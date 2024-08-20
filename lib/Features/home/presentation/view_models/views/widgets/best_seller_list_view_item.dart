1c6430b7552b0158d70f0178d8b8e1d09c30fb32
Mohammed Samy <mom118580@gmail.com>
2024-08-20 00:09:02 -0700

    best seller list view item : part 3

--- lib/Features/home/presentation/view_models/views/widgets/best_seller_list_view_item.dart
+++ lib/Features/home/presentation/view_models/views/widgets/best_seller_list_view_item.dart
@@ -1,3 +1,4 @@
+import 'package:booky_app_clean_arctect/constants.dart';
 import 'package:booky_app_clean_arctect/core/utils/assets_data.dart';
 import 'package:booky_app_clean_arctect/core/utils/styles.dart';
 import 'package:flutter/material.dart';
@@ -30,18 +31,38 @@ class BestSellerListViewItem extends StatelessWidget {
             width: 30,
           ),
           Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 width: MediaQuery.of(context).size.width * 0.5,
                 child: Text(
                   'Harry Potter and the Goblet of Fire',
-                  style: Styles.textStyle20,
+                  style: Styles.textStyle20.copyWith(fontFamily: kGTSectraFine),
                   maxLines: 2,
                   overflow: TextOverflow.ellipsis,
                 ),
               ),
+              const SizedBox(
+                height: 3,
+              ),
+              Text(
+                'J.K. Rowling',
+                style: Styles.textStyle14,
+              ),
+              const SizedBox(
+                height: 3,
+              ),
+              Row(
+                children: [
+                  Text(
+                    '19.99 \$',
+                    style: Styles.textStyle20
+                        .copyWith(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              )
             ],
-          )
+          ),
         ],
       ),
     );
