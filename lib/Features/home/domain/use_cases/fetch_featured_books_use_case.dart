3f24a6dba56f19178aa726f85fa5710a0a839d82
Mohammed Samy <mom118580@gmail.com>
2024-08-21 01:09:23 -0700

    refactore to Use case class

--- lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -1,15 +1,22 @@
 import 'package:booky_app_clean_arctect/Features/home/domain/entities/book_entity.dart';
 import 'package:booky_app_clean_arctect/Features/home/domain/repos/home_repo.dart';
 import 'package:booky_app_clean_arctect/core/errors/failure.dart';
+import 'package:booky_app_clean_arctect/core/use_case/use_case.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchFeaturedBooksUseCase {
+class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
   final HomeRepo homeRepo;
 
   FetchFeaturedBooksUseCase({required this.homeRepo});
 
-  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
-    // check permssion
-    return homeRepo.fetchFeaturedBooks();
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) {
+    // TODO: implement call
+    throw UnimplementedError();
   }
+
+  // Future<Either<Failure, List<BookEntity>>> call() {
+  //   // check permssion
+  //   return homeRepo.fetchFeaturedBooks();
+  // }
 }
