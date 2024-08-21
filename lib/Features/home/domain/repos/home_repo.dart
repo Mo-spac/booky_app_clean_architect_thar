f85b08d5e9d91bee06bc2733ce5cd861ec0647e1
Mohammed Samy <mom118580@gmail.com>
2024-08-20 23:49:01 -0700

    Using Either class by dartZ package

--- lib/Features/home/domain/repos/home_repo.dart
+++ lib/Features/home/domain/repos/home_repo.dart
@@ -1,6 +1,8 @@
 import 'package:booky_app_clean_arctect/Features/home/domain/entities/book_entity.dart';
+import 'package:booky_app_clean_arctect/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<List<BookEntity>> fetchFeaturedBooks();
-  Future<List<BookEntity>> fetchNewestBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks();
 }
