23adb5934a5bec50cc1566f462f8c47b8a7fb3b1
Mohammed Samy <mom118580@gmail.com>
2024-08-22 22:31:03 -0700

    Generate book entity adabter

--- lib/Features/home/domain/entities/book_entity.dart
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -1,9 +1,18 @@
+import 'package:hive/hive.dart';
+part "book_entity.g.dart";
+
+@HiveType(typeId: 0)
 class BookEntity {
+  @HiveField(0)
   final String bookId;
+  @HiveField(1)
   final String? image;
+  @HiveField(2)
   final String title;
+  @HiveField(3)
   final String? authorName;
-  final num? price;
+  @HiveField(4)
+  final int? price;
   // final num? rating;
 
   BookEntity({
